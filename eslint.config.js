fbda89877de36a52afe41c9b86855dad52cc3e59
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-08-10 16:41:49 +0700

    init eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import antfu from '@antfu/eslint-config'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import nuxt from './.nuxt/eslint.config.mjs'
+
+export default antfu(
+  {
+    rules: {
+      'style/key-spacing': 'off',
+      'style/object-curly-spacing': 'off',
+      'antfu/if-newline': 'off',
+      'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
+      'style/brace-style': ['error', '1tbs'],
+      'style/arrow-parens': 'off',
+      'style/operator-linebreak': 'off',
+    },
+  },
+  nuxt,
+  eslintPluginPrettierRecommended,
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  modules: ['@nuxt/eslint',],
+  css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
