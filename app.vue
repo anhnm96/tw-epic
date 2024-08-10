6ebe45a54378c27b258ebb704ba5d59673af8775
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-08-10 19:39:27 +0700

    dynamic layout containers

--- app.vue
+++ app.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <EpicStackLogo class="size-20" />
-    <h1>The <span class="text-highlight">Epic</span> Stack</h1>
-    <p>
-      Check the <a href="#">Getting Started</a> guide file for how to get your
-      project off the ground!
-    </p>
-    <Logos />
+  <div class="grid h-dvh place-items-center px-4 py-16">
+    <div class="grid place-items-center gap-12">
+      <div>
+        <EpicStackLogo class="size-20" />
+        <h1>The <span class="text-highlight">Epic</span> Stack</h1>
+        <p>
+          Check the <a href="#">Getting Started</a> guide file for how to get
+          your project off the ground!
+        </p>
+      </div>
+      <Logos />
+    </div>
   </div>
 </template>
